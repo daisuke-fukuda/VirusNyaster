ff0cab95421f52ab6b460828300a4a29acef14ee
daisuke-fukuda <32258949+daisuke-fukuda@users.noreply.github.com>
2020-02-29 20:14:37 +0900

    iconクリックしたらcontentのscriptが走る

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -1,1 +1,29 @@
-console.log('test');
+
+const exe = () => {
+  console.log('exe');
+}
+
+
+// popupからのmessage
+const addListenerFromPopup = () => {
+  // eslint-disable-next-line no-unused-vars
+  chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    // console.log('chrome.runtime.onMessage.addListener', msg);
+
+    switch (msg.command) {
+      // ツールバー横のアイコンクリック
+      case 'start':
+        exe();
+        break;
+      default:
+        console.warn('invalid command');
+    }
+  });
+};
+
+
+window.onload = () => {
+  addListenerFromPopup();
+};
+
+
--- extension/src/js/popup.js
+++ extension/src/js/popup.js
@@ -1,4 +1,6 @@
 import '../css/popup.css';
-import hello from './popup/example';
 
-hello();
+// https://stackoverflow.com/questions/20019958/chrome-extension-how-to-send-data-from-content-script-to-popup-html
+chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+  chrome.tabs.sendMessage(tabs[0].id, {command: "start"});
+});
