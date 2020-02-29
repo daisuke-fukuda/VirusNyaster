ff5df27aef2fbd0c02d8d55826b853146043752f
daisuke-fukuda <32258949+daisuke-fukuda@users.noreply.github.com>
2020-02-29 21:31:57 +0900

    画像追加

--- extension/src/js/background.js
+++ extension/src/js/background.js
@@ -3,3 +3,8 @@ import '../img/icon-19.png';
 import '../img/icon-38.png';
 import '../img/icon-48.png';
 import '../img/icon-128.png';
+import '../img/cat1.jpg';
+import '../img/cat2.jpg';
+import '../img/cat3.jpg';
+import '../img/cat4.jpg';
+import '../img/cat5.jpg';
